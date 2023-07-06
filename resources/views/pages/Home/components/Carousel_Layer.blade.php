9cb4715c09cba16060fdf0f142075018962ae374
SadeeshaChiran <pmsckaushalya@gmail.com>
2023-07-06 12:26:38 +0530

    mistake

--- resources/views/pages/Home/components/Carousel_Layer.blade.php
+++ resources/views/pages/Home/components/Carousel_Layer.blade.php
@@ -3,7 +3,7 @@
         <div class="mb-2 col-12 d-flex">
 
             <h2 class="text-6xl font-semibold position-absolute" style="margin-top: 25vh; margin-left: 80px;">UP TO 50% OFF</h2>
-            <h2 class="font-bold text-orange-500 position-absolute text-9xl" style="margin-top: 33vh; margin-left: 80px;">Shoes For All</h2>
+            <h2 class="font-bold text-orange-500 position-absolute text-9xl" style="margin-top: 33vh; margin-left: 80px;">DRONE For All</h2>
         </div>
         <button type="button" class="border-0 rounded-lg shadow-lg col-2 btn btn-dark position-absolute text-dark hover:bg-sky-700" style="margin-top: 55vh; margin-left: 100px; height:50px">SHOP NOW! <i class="fa-solid fa-arrow-right fa-beat ms-3" style="color: #000000;"></i></button>
 
@@ -26,7 +26,7 @@
         <div class="text-center col-12">
 
             <h2 class="text-2xl font-semibold ">UP TO 50% OFF</h2>
-            <h2 class="text-5xl font-bold text-orange-500 ">Shoes For All</h2>
+            <h2 class="text-5xl font-bold text-orange-500 ">DRONE For All</h2>
         </div>
 
         <div class="mt-3 mb-3 col-10 offset-1">
